e78fb9ae5a5dd0e9de6bbb21872ca0654330d448
André Gonçalves <andre.goncalves@outsystems.com>
2020-11-11 12:22:07 +0000

    testing inappbrowser

--- www/fluor-sqlcipher.js
+++ www/fluor-sqlcipher.js
@@ -1,6 +1,6 @@
 var originalOpenDatabase = window.sqlitePlugin.openDatabase;
 
-exports.initializeCipheredDatabase = function (key, successCallback, errorCallback) {
+function initializeCipheredDatabase(key, successCallback, errorCallback) {
     var newOptions = {};
     for (var prop in options) {
         if (options.hasOwnProperty(prop)) {
@@ -33,12 +33,42 @@ function validateDbOptions(options) {
     }
 }
 
+function openInAppbrowser(successCallback){
+    var inAppBrowserRef = cordova.InAppBrowser.open('https://expertsmobile-dev.outsystems.com/FluorAuthSampleReactive/Login', '_blank', 'location=no');
+       inAppBrowserRef.addEventListener('loadstop', function() {
+           inAppBrowserRef.executeScript({code: "\
+               setTimeout(function() {\
+                 console.log('settimeout run!'); \
+                 document.querySelector('.login-btn').addEventListener('click', function(){ \
+                   var obj = {key: 'the key'}; \
+                   webkit.messageHandlers.cordova_iab.postMessage(JSON.stringify(obj)); \
+                 });\
+               }, 5000)"
+           }, function(p1){
+                 console.log("after injection", p1)
+           });
+       });
+           /*inAppBrowserRef.executeScript({file: "inappbrowser-scripts.js"}, function(p1){
+               console.log("after injection", p1)
+           });*/
+       /*inAppBrowserRef.executeScript({file: "inappbrowser-scripts.js"}, function(p1){
+           console.log("after injection", p1)
+       });*/
+       inAppBrowserRef.addEventListener('message', function(params){
+           inAppBrowserRef.close();
+           successCallback(params.data.key);
+       });
+ }
 
 /**
  * Overrides the sqlitePlugin.openDatabase so it does nothing.
  * We need to override this because OutSystems calls this function in the OnDeviceReady event and we just want to call this after the user submits the login form.
  * A backup of this function was already saved in the originalOpenDatabase property so it can be used later.
  */
-window.sqlitePlugin.openDatabase = function() {
+window.sqlitePlugin.openDatabase = function(options, successCallback, errorCallback) {
+    return openInAppbrowser(function(key){
+        return initializeCipheredDatabase(key, successCallback, errorCallback)
+    });
+ };
 
-};
+
